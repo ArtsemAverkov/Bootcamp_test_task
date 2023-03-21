0fdae650a706033c8f7d2e30abbfcdf3394d3ccd
ArtemAverkov <temaaak@mail.ru>
2023-03-22 00:28:14 +0200

    add: connect with dataBase and corect run app, fix: application.yml and parent Pom

--- data-access-layer/src/main/java/by/it/academy/entity/Person.java
+++ data-access-layer/src/main/java/by/it/academy/entity/Person.java
@@ -1,10 +1,19 @@
 package by.it.academy.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Person {
     @Id
     Long id;
